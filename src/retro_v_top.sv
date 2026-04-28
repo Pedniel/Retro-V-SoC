6fd8660731ad9df229a17cc4e4f3e736a36e4d0e
Daniel Pedder <d.pedder@hm.edu>
2026-04-28 21:16:48 +0200

    Add riscv core registers according to spec
    Add registers according to:
    
    https://docs.riscv.org/reference/isa/unpriv/rv32.html

--- src/retro_v_top.sv
+++ src/retro_v_top.sv
@@ -2,4 +2,9 @@ module retro_v_top (
     input wire instruction [31:0]
     );
 
+   reg [31:0] x0, x1, x2, x4, x5, x6, x7, x8, x9, x10, z11,
+              x12, x13, x14, x15, x16, x17, x18, x19, x20,
+              x21, x22, x23, x24, x25, x26, x27, x28, x29,
+              x30, x31, pc;
+   
 endmodule  
